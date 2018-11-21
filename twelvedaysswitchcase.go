49418e661c1e1cbe9648c684eb46bffdda56eebf
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-11-21 11:01:00 -0800

    Add files via upload

---
+++ twelvedaysswitchcase.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func main() {
+	for day := 1; day <= 12; day++ {
+		fmt.Println("on the", day, "of christmas my true love sent to me")
+
+		switch day {
+		case 12:
+			fmt.Println("twelve drummers drumming")
+		case 11:
+			fmt.Println("Eleven pipers piping")
+		case 10:
+			fmt.Println("ten lords a leaping")
+		case 9:
+			fmt.Println("nine ladies dancing")
+		case 8:
+			fmt.Println("eight maids a milking")
+		case 7:
+			fmt.Println("seven swans swimming")
+		case 6:
+			fmt.Println("six geese a laying")
+		case 5:
+			fmt.Println("five golden rings")
+		case 4:
+			fmt.Println("four calling birds")
+		case 3:
+			fmt.Println("three french hens")
+		case 2:
+			fmt.Println("two turtle doves")
+		case 1:
+			fmt.Println("a partridge in fear tree.")
+		}
+
+		if day == 12 {
+
+		} else if day == 11 {
+
+		} else if day == 10 {
+
+		} else if day == 9 {
+
+		} else if day == 8 {
+
+		} else if day == 7 {
+
+		} else if day == 6 {
+
+		} else if day == 5 {
+
+		} else if day == 4 {
+
+		} else if day == 3 {
+
+		} else if day == 2 {
+
+		} else if day == 1 {
+
+		}
+	}
+
+}
